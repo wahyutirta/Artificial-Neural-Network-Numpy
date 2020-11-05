da3dd64f594d3b7e94fb033b74446fa44f162374
molent4 <57343997+molent4@users.noreply.github.com>
2020-11-06 00:00:42 +0800

    fix data bug

--- data.py
+++ data.py
@@ -16,11 +16,11 @@ class spiral():
         y = np.zeros(samples*classes, dtype='uint8')
         for class_number in range(classes):
             ix = range(samples*class_number, samples*(class_number+1))
-            r = np.linspace(0.0, 1, samples)
+            r = np.linspace(0.0, 1, samples)  # radius
             t = np.linspace(class_number*4, (class_number+1)*4, samples) + np.random.randn(samples)*0.2
             X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
             y[ix] = class_number
-            return X, y
+        return X, y
 
 class vertical():
     def create_data(samples, classes):
